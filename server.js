6a5581869ae84337ccd73277246ab0588c43150c
Rob Hess <hessro@oregonstate.edu>
2024-11-20 15:54:19 -0800

    Add code from 11/20 lecture.

--- server.js
+++ server.js
@@ -24,10 +24,34 @@ app.get("/cats", function (req, res, next) {
   res.status(200).send("<html><body><h1>Cats</h1></body></html>")
 })
 
+var availablePeople = [
+  "leia",
+  "luke",
+  "rey",
+  "finn",
+  "r2d2"
+]
+app.get("/people/:person", function (req, res, next) {
+  console.log("  -- Person route, req.params:", req.params)
+  var person = req.params.person
+  if (availablePeople.indexOf(person) !== -1) {
+    var file = __dirname + "/static/people/" + person + ".html"
+    res.status(200).sendFile(file)
+  } else {
+    next()
+  }
+})
+
+app.use(express.static("static"))
+
 // app.post()
 // app.patch()
 // app.put()
 // app.delete()
+
+app.get("*", function (req, res, next) {
+  res.status(404).sendFile(__dirname + "/static/404.html")
+})
 
 app.listen(3001, function () {
   console.log("== Server listening for requests on port 3001")
